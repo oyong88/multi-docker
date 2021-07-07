b89f6970599e518ba5dff07fadde515ec00abd6a
AzureAD\오용택 <oyong@zempot.com>
2021-07-07 17:58:36 +0900

    init

---
+++ client/src/OtherPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+ const OtherPage = () => {
+    return (
+        <div>
+            I'm some other page
+            <Link to="/">Go to Home</Link>
+        </div>
+    );
+}
+
+export default OtherPage;
+
